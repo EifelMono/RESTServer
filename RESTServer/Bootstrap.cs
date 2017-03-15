a3d01a56b785c9cd94db430ef69b12b7d0e234c5
Andreas Klapperich <andreas@klapperich.de>
2017-03-15 21:53:15 +0100

    RestServer with Swagger

--- RESTServer/Bootstrap.cs
+++ RESTServer/Bootstrap.cs
@@ -1,10 +1,47 @@
-﻿using System;
-namespace RESTServer
+﻿using System.Runtime.InteropServices.ComTypes;
+using Nancy.Conventions;
+using Nancy.Swagger.Annotations;
+using Nancy.Swagger.Services;
+using Swagger.ObjectModel;
+
+namespace RestServer
 {
-    public class Bootstrap
-    {
-        public Bootstrap()
-        {
-        }
-    }
+    using Nancy;
+    using Nancy.Bootstrapper;
+    using Nancy.TinyIoc;
+
+    public class Bootstrapper : DefaultNancyBootstrapper
+	{
+		protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+		{
+			SwaggerMetadataProvider.SetInfo("RESTServer Example with doc", "v0", "RESTServer service", new Contact()
+			{
+				EmailAddress = "andreas@klapperich.de"
+			});
+
+			base.ApplicationStartup(container, pipelines);
+
+			// SwaggerAnnotationsConfig.ShowOnlyAnnotatedRoutes = true;
+		}
+
+		protected override void ConfigureConventions(NancyConventions nancyConventions)
+		{
+			base.ConfigureConventions(nancyConventions);
+
+			nancyConventions.StaticContentsConventions.Add(
+				StaticContentConventionBuilder.AddDirectory("/swagger-ui/dist")
+			);
+		}
+
+		/// <summary>
+		/// Initialise the request - can be used for adding pre/post hooks and
+		///             any other per-request initialisation tasks that aren't specifically container setup
+		///             related
+		/// </summary>
+		/// <param name="container">Container</param><param name="pipelines">Current pipelines</param><param name="context">Current context</param>
+		protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
+		{
+			pipelines.AfterRequest.AddItemToEndOfPipeline(x => x.Response.Headers.Add("Access-Control-Allow-Origin", "*"));
+		}
+	}
 }
--- RESTServer/Program.cs
+++ RESTServer/Program.cs
@@ -4,32 +4,42 @@ using Nancy.Hosting.Self;
 using Nancy.ModelBinding;
 using RESTLib;
 using System.Threading;
+using System.Runtime.Remoting.Messaging;
+using System.Data.SqlTypes;
 
 namespace RESTServer
 {
     public class Module : NancyModule
     {
+
         public Module()
             : base()
         {
-            Post["/Hello"] = _ =>
+            Get("/", _ => Response.AsRedirect("/swagger-ui/dist/index.html"), null, "Home");
+
+            // Get("/swagger-ui/dist/index.html", _ => Response.AsFile("swagger-ui/dist/index.html"), null, "Home");
+
+            Post("/Hello", (_) =>
             {
                 var request = this.Bind<HelloRequest>();
                 return Response.AsJson(new HelloResponse
-                    {
-                        Text = request.Text + " => " + DateTime.Now.ToString("HH:mm:ss"),
-                        Id = request.Id + 1,
-                    });
-            };
+                {
+                    Text = request.Text + " => " + DateTime.Now.ToString("HH:mm:ss"),
+                    Id = request.Id + 1,
+                });
+            });
 
-            Get["/user/{id}"] = parameters =>
-            {
-                if (((int)parameters.id) == 666)
-                    return $"Blocked user #{parameters.id}!";
-                else
-                    return "Regular user!";
-            };
+            Get("/user/{id}", (parameters) => UserId(parameters));
         }
+
+        public static string UserId(dynamic parameters)
+        {
+            if (((int)parameters.id) == 666)
+                return $"Blocked user #{parameters.id}!";
+            else
+                return "Regular user!";
+        }
+
     }
 
     class Program
